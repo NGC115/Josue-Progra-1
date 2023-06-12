918004e8a65eafe44909ca37bd6f2b89edd6201b
NGC115 <J_Josue05@outlook.com>
2023-06-12 09:00:54 -0600

    008
    app de escritorio

---
+++ P017 Matrices/P017 Matrices.cpp
@@ -0,0 +1,19 @@
+// P017 Matrices.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+
+
+#include <iostream>
+#include <random>
+
+int main()
+{
+    int matriz[3][3];
+    
+    
+}
+
+
+    
+
+    
